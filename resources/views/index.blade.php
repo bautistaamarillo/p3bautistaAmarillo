e2c6906aa588a6819e71b517bd3c30e1ace2041a
bautistaamarillo <amarillobautista5@gmail.com>
2023-03-30 19:14:04 -0300

    primer ejercicio resuelto. (Boton de volver)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,12 @@
 Hola mundo!
+<hr>
+<br>
+<h1>Bienvenido.</h1>
+<li>Bautista Amarillo
+</li>
+<br>
+<hr>
+<!-- <input type="button" value="Volver" onclick="volver()"> -->
+
+<a href="/"><button> Volver </button></a>
+Armar un boton que diga volver, y que ese boton nos lleve a la pagina de welcome
