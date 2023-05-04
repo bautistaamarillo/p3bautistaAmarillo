55139bfed0ed5449ed5f297cc81f471cec750fe3
bautistaamarillo <amarillobautista5@gmail.com>
2023-05-04 18:12:12 -0300

    login y seeder de user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 use  App\Models\Student;
+use  App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,9 @@ class DatabaseSeeder extends Seeder
     {   
         $this->call([StudentSeeder::class]);
        Student::factory(100)->create();
+
+       $this->call([UserSeeder::class]);
+       User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AuditController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +14,37 @@ use App\Http\Controllers\AuditController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-// */
+*/
+//--------- INDEX -------------
 
-// Route::get('test', function () {
-//     return view('welcome');
-// });
 Route::get('index', function () {
     return view('index');
 });
+
+//--------- Student CRUD -------------
+
 Route::resource('students',StudentController::class);
 
+//--------- AUDIT -------------
+
 Route::get('audit', [AuditController::class, 'index']);
+
+// --------- WELCOME ------------
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// --------- LOGIN ------------
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
